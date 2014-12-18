9c2b05a7f18f99218b08e2880487e5129dd30c88
Siggi Dünkel <chaosprinz76@googlemail.com>
2014-12-18 21:01:49 +0100

    Adding and configuring zsh using oh-my-zsh

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,4 +1,22 @@
+# install some basic npm-packages
 include_recipe 'nodejs::npm'
 
 nodejs_npm 'grunt-cli'
 nodejs_npm 'coffee-script'
+
+# setup zsh
+
+node.default[:oh_my_zsh][:users] = [
+  {
+    login: 'vagrant',
+    theme: 'gnzh',
+    plugins: ['git', 'npm', 'bower', 'coffee', 'node']
+  },
+  {
+    login: 'root',
+    theme: 'gnzh',
+    plugins: ['git', 'npm', 'bower', 'coffee', 'node']
+  }
+]
+
+include_recipe 'oh_my_zsh'

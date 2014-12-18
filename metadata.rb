b316384e4b8ebddb56a92d22dc0e1aee88b4a162
Siggi Dünkel <chaosprinz76@googlemail.com>
2014-12-18 12:03:02 +0100

    Added cookbooks metadata

---
+++ metadata.rb
@@ -0,0 +1,9 @@
+name 'nodejs-dev-env-setup'
+maintainer 'chaosprinz'
+maintainer_email 'chaosprinz76@googlemail.com'
+license 'MIT'
+description 'Setting up a nodejs-development environment'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version '0.1.0'
+
+depends 'nodejs'
